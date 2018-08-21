f697648f36a1dc21a427388340b8189435405ae7
LymiaH <LymiaH@users.noreply.github.com>
2018-08-21 12:51:07 +0800

    Add default django authentication pages

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Automatic grader for programming assignments.
 ## Running
 - pip install -r requirements.txt
 - python manage.py migrate
-- python manage.py startserver
+- cp decentsite/local_settings_example.py decentsite/location_settings.py
+- python manage.py runserver --settings decentsite.local_settings
 
 ## References
-- [django](https://www.djangoproject.com/)
+- [django](https://www.djangoproject.com/) the web framework and its examples
 - [gitignore.io](https://www.gitignore.io/)
+- [django-todo](https://github.com/shacker/django-todo) for its examples
---
+++ decentsite/local_settings_example.py
@@ -0,0 +1,14 @@
+from decentsite.settings import *
+
+# Generate a new key with:
+# from django.core.management.utils import get_random_secret_key
+# get_random_secret_key()
+# Source: https://github.com/django/django/blob/d46bf119799f4d319f41d890366bc8154017b432/django/core/management/utils.py#L76
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'm!388n2g@k%r&y)f6)pw!3=x==qn10_(+75b-hmd8&#ghq4qg@'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: Comment this out for production
+AUTH_PASSWORD_VALIDATORS = []
